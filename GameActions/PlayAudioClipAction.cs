16ada0d51eda4bdb3b99655b2e1716e35fc5b192
Majidzadeh <hashpragmaonce@gmail.com>
2020-02-01 15:21:20 +0330

    Add multiple AudioClip support to PlayAudioClipAction

--- GameActions/PlayAudioClipAction.cs
+++ GameActions/PlayAudioClipAction.cs
@@ -5,15 +5,28 @@ namespace GameActions
 {
     public class PlayAudioClipAction : GameActionWithTargetObject
     {
-        public AudioClip AudioClip = null;
+        [Tooltip(
+                "The AudioClips to play using the AudioSource on the target Object. "
+                + "Will just play the AudioSource if empty."
+            )]
+        public AudioClip[] AudioClips = {};
+        public bool PlayRandomAudioClip = true;
+
+        private int CurrentIndex = 0;
 
         protected override Task Act(ActParameters Parameters)
         {
             AudioSource AudioSource = Parameters.Object.GetComponent<AudioSource>();
             if (AudioSource != null)
             {
-                if (AudioClip != null)
-                    AudioSource.PlayOneShot(AudioClip);
+                if (AudioClips != null && AudioClips.Length > 0)
+                {
+                    if (PlayRandomAudioClip)
+                        CurrentIndex = Random.Range(0, AudioClips.Length);
+                    else
+                        CurrentIndex = (CurrentIndex + 1) % AudioClips.Length;
+                    AudioSource.PlayOneShot(AudioClips[CurrentIndex]);
+                }
                 else
                     AudioSource.Play();
             }
